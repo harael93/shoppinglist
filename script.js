1312444c7409bd486c39204aadcd6f7b412099a5
harael93 <liquidsoliddesign@gmail.com>
2025-07-06 16:37:22 -0400

    now builds the list

---
+++ script.js
@@ -0,0 +1,9 @@
+function submit() {
+    let newitem = document.createElement("li"); 
+    newitem.textContent = document.getElementById('items').value;
+    document.getElementById('list').appendChild(newitem);
+    document.getElementById('items').value = '';
+}
+
+document.getElementById('newUl');
+addEventListener('click', submit);
